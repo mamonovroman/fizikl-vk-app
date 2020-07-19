ae317f2f7f9ebdd630308739823b04355cc0a3fd
Mikhail Katsupko <t63723@gmail.com>
2020-07-19 14:17:13 +0300

    Updates

---
+++ precache-manifest.0b99190d6707399e15f11cf820e6dca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771e6055a024e4d6b1f0bc707c59679a",
+    "url": "/pt-main-vk-app/index.html"
+  },
+  {
+    "revision": "476c9fa39843afddee55",
+    "url": "/pt-main-vk-app/static/css/2.ed87aa75.chunk.css"
+  },
+  {
+    "revision": "476c9fa39843afddee55",
+    "url": "/pt-main-vk-app/static/js/2.6ba20625.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pt-main-vk-app/static/js/2.6ba20625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cbc4672bd5806ad52e",
+    "url": "/pt-main-vk-app/static/js/main.8e42d2a7.chunk.js"
+  },
+  {
+    "revision": "59926e1b5a012b5d4b1f",
+    "url": "/pt-main-vk-app/static/js/runtime-main.a8d029d6.js"
+  }
+]);
